bfe0defd300e6d87dc1d484a3a8440799b7935df
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-08-10 14:08:33 +0530

    feat: enhance location features and add search functionality in Answer component

--- app/(drawer)/(tabs)/assigment.jsx
+++ app/(drawer)/(tabs)/assigment.jsx
@@ -1,173 +1,252 @@
 import React, { useState } from "react";
-import { View, Button, Text, Alert,ScrollView } from "react-native";
+import {
+  View,
+  Button,
+  Text,
+  Alert,
+  ScrollView,
+  TextInput,
+} from "react-native";
 import * as Location from "expo-location";
 
 const Answer = () => {
-    const [crrlocation, setcrrlocation] = useState(null);
-    const [lastlocationn, setlastLocationn] = useState(null);
-    const [address, setaddress] = useState(null);
+  const [crrlocation, setcrrlocation] = useState(null);
+  const [lastlocationn, setlastLocationn] = useState(null);
+  const [address, setaddress] = useState(null);
 
-    const handlepermission = async () => {
-        const permission = await Location.requestForegroundPermissionsAsync();
+  const [search, setsearch] = useState("");
+  const [searchedLocation, setsearchedLocation] = useState(null);
 
-        if (!permission.granted) {
-            Alert.alert("Permission Denied", "Please allow location permission.");
-            return;
-        }
+  const handlepermission = async () => {
+    const permission = await Location.requestForegroundPermissionsAsync();
 
-        Alert.alert("Permission Granted");
-        console.log("Permission accessed");
-    };
-
-    const CurrentLocation = async () => {
-        const permission = await Location.requestForegroundPermissionsAsync();
-
-        if (!permission.granted) {
-            Alert.alert("Permission Required");
-            return;
-        }
-
-        const position = await Location.getCurrentPositionAsync({});
-        setcrrlocation(position);
-
-
-        console.log(position);
-    };
-
-    const lastlocation = async () => {
-        const permission = await Location.requestForegroundPermissionsAsync();
-
-        if (!permission.granted) {
-            return;
-        }
-
-        const llastlocation = await Location.getLastKnownPositionAsync({});
-
-        if (!llastlocation) {
-            return (
-                <View>
-                    <Text>No Last Known Location Found</Text>
-                </View>
-            )
-        }
-        console.log(llastlocation);
-
-        setlastLocationn(llastlocation);
+    if (!permission.granted) {
+      Alert.alert("Permission Denied", "Please allow location permission.");
+      return;
     }
 
-    const currentaddress = async () => {
-        const permission = await Location.requestForegroundPermissionsAsync();
+    Alert.alert("Permission Granted");
+    console.log("Permission accessed");
+  };
 
-        if (!permission.granted) {
-            alert("no permission granted");
-            return;
-        }
+  const CurrentLocation = async () => {
+    const permission = await Location.requestForegroundPermissionsAsync();
 
-        const temp = await Location.reverseGeocodeAsync({
-            latitude: crrlocation.coords.latitude,
-            longitude: crrlocation.coords.longitude,
-        });
-
-        setaddress(temp);
-        console.log(temp)
+    if (!permission.granted) {
+      Alert.alert("Permission Required");
+      return;
     }
-    return (
-        <ScrollView >
-            <Button
-                title="Request Permission"
-                onPress={handlepermission}
-            />
 
-            <View style={{ height: 20 }} />
+    const position = await Location.getCurrentPositionAsync({});
+    setcrrlocation(position);
 
-            <Button
-                title="Get Current Location"
-                onPress={CurrentLocation}
-            />
+    console.log(position);
+  };
 
-            <View style={{ height: 20 }} />
+  const lastlocation = async () => {
+    const permission = await Location.requestForegroundPermissionsAsync();
 
+    if (!permission.granted) {
+      return;
+    }
 
-            <Button
-                title="Get Last Known Location"
-                onPress={lastlocation}
-            />
+    const llastlocation = await Location.getLastKnownPositionAsync({});
 
-            <View style={{ height: 20 }} />
+    if (!llastlocation) {
+      Alert.alert("No Last Known Location Found");
+      return;
+    }
 
-            <Button
-                title="print Address"
-                onPress={currentaddress}
-            />
-            <Button 
-            title="refresh"
-            onPress={CurrentLocation}
-            />
-            {crrlocation && (
-                <>
-                    <Text style={{ color: "white", marginTop: 20 }}>
-                        Latitude: {crrlocation.coords.latitude}
-                    </Text>
+    console.log(llastlocation);
 
-                    <Text style={{ color: "white" }}>
-                        Longitude: {crrlocation.coords.longitude}
-                    </Text>
+    setlastLocationn(llastlocation);
+  };
 
-                    <Text style={{ color: "white", }}>
-                        acurracy: {crrlocation.coords.accuracy}
-                    </Text>
+  const currentaddress = async () => {
+    const permission = await Location.requestForegroundPermissionsAsync();
 
-                    <Text style={{ color: "white", }}>
-                        Heading: {crrlocation.coords.heading}
-                    </Text>
-                    <Text style={{ color: "white", }}>
-                        Timestamp: {crrlocation.timestamp}
-                    </Text>
+    if (!permission.granted) {
+      alert("No permission granted");
+      return;
+    }
 
-                </>
-            )}
-            {lastlocationn && (
-                <>
-                    <Text style={{ color: "white", marginTop: 20 }}>
-                        Latitude: {lastlocationn.coords.latitude}
-                    </Text>
+    if (!crrlocation) {
+      alert("Get current location first");
+      return;
+    }
 
-                    <Text style={{ color: "white" }}>
-                        Longitude: {lastlocationn.coords.longitude}
-                    </Text>
-                </>
-            )}
-            {address && (
-                <>
-                    <Text style={{ color: "white", marginTop: 20 }}>
-                        Name: {address[0]?.name}
-                    </Text>
+    const temp = await Location.reverseGeocodeAsync({
+      latitude: crrlocation.coords.latitude,
+      longitude: crrlocation.coords.longitude,
+    });
 
-                    <Text style={{ color: "white" }}>
-                        Street: {address[0]?.street}
-                    </Text>
-                    <Text style={{ color: "white", }}>
-                        City: {address[0]?.city}
-                    </Text>
+    setaddress(temp);
+    console.log(temp);
+  };
 
-                    <Text style={{ color: "white" }}>
-                        District: {address[0]?.district}
-                    </Text>
-                    <Text style={{ color: "white", }}>
-                        state: {address[0]?.region}
-                    </Text>
+  const findsearch = async () => {
+    const permission = await Location.requestForegroundPermissionsAsync();
 
-                    <Text style={{ color: "white" }}>
-                        Country: {address[0]?.country}
-                    </Text>
-                     <Text style={{ color: "white" }}>
-                        Postal Code: {address[0]?.postalCode}
-                    </Text>
+    if (!permission.granted) {
+      alert("Permission denied");
+      return;
+    }
 
-                </>
-            )}
-        </ScrollView>
-    );
+    const finded = await Location.geocodeAsync(search);
+
+    if (finded.length === 0) {
+      alert("Location not found");
+      return;
+    }
+
+    setsearchedLocation(finded[0]);
+
+    console.log(finded);
+  };
+
+  return (
+    <ScrollView style={{ flex: 1, backgroundColor: "black", padding: 20 }}>
+      <Button
+        title="Request Permission"
+        onPress={handlepermission}
+      />
+
+      <View style={{ height: 20 }} />
+
+      <Button
+        title="Get Current Location"
+        onPress={CurrentLocation}
+      />
+
+      <View style={{ height: 20 }} />
+
+      <Button
+        title="Get Last Known Location"
+        onPress={lastlocation}
+      />
+
+      <View style={{ height: 20 }} />
+
+      <Button
+        title="Print Address"
+        onPress={currentaddress}
+      />
+
+      <View style={{ height: 20 }} />
+
+      <Button
+        title="Refresh Current Location"
+        onPress={CurrentLocation}
+      />
+
+      <View style={{ height: 20 }} />
+
+      <TextInput
+        placeholder="Enter State Name"
+        placeholderTextColor="gray"
+        value={search}
+        onChangeText={(text) => setsearch(text)}
+        style={{
+          borderWidth: 1,
+          borderColor: "white",
+          color: "white",
+          padding: 10,
+          borderRadius: 5,
+        }}
+      />
+
+      <View style={{ height: 15 }} />
+
+      <Button
+        title="Search Latitude & Longitude"
+        onPress={findsearch}
+      />
+
+      {crrlocation && (
+        <>
+          <Text style={{ color: "white", marginTop: 20 }}>
+            Latitude: {crrlocation.coords.latitude}
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            Longitude: {crrlocation.coords.longitude}
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            Accuracy: {crrlocation.coords.accuracy}
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            Heading: {crrlocation.coords.heading}
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            Timestamp: {crrlocation.timestamp}
+          </Text>
+        </>
+      )}
+
+      {lastlocationn && (
+        <>
+          <Text style={{ color: "white", marginTop: 20 }}>
+            Last Latitude: {lastlocationn.coords.latitude}
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            Last Longitude: {lastlocationn.coords.longitude}
+          </Text>
+        </>
+      )}
+
+      {address && (
+        <>
+          <Text style={{ color: "white", marginTop: 20 }}>
+            Name: {address[0]?.name}
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            Street: {address[0]?.street}
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            City: {address[0]?.city}
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            District: {address[0]?.district}
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            State: {address[0]?.region}
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            Country: {address[0]?.country}
+          </Text>
+q
+          <Text style={{ color: "white" }}>
+            Postal Code: {address[0]?.postalCode}
+          </Text>
+        </>
+      )}
+
+      {searchedLocation && (
+        <>
+          <Text style={{ color: "white", marginTop: 20 }}>
+            Search Result
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            Latitude: {searchedLocation.latitude}
+          </Text>
+
+          <Text style={{ color: "white" }}>
+            Longitude: {searchedLocation.longitude}
+          </Text>
+        </>
+      )}
+    </ScrollView>
+  );
 };
 
 export default Answer;
