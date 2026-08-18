ce67200316aa7b79696afeaadcc24abc7b5f4d70
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-08-18 11:02:12 +0530

    feat: enhance ClipboardExample with text input and retrieval functionality

--- app/(drawer)/(tabs)/clipboard.jsx
+++ app/(drawer)/(tabs)/clipboard.jsx
@@ -1,25 +1,45 @@
-import { Button, Text, View } from 'react-native';
-import React from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
 import * as Clipboard from 'expo-clipboard';
 
 const ClipboardExample = () => {
-  const handleCopyText = async () => {
-    await Clipboard.setStringAsync('hello');
-  };
+    const [text, settext] = useState(null);
+    const [copied, setcopied] = useState(null);
+    const handleCopyText = async () => {
+        await Clipboard.setStringAsync(text);
+        console.log({ text });
+    };
+    const handleGetCopiedText = async () => {
+        const check=await Clipboard.hasStringAsync();
+        const res = await Clipboard.getStringAsync();
 
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'aqua',
-      }}
-    >
-      <Text>hello</Text>
-      <Button title="Copy Text" onPress={handleCopyText} />
-    </View>
-  );
+        if(check){
+             setcopied(res);
+        }
+        else{
+            alert("nothing to paste")
+        }
+        console.log(res);
+       
+    }
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'aqua',
+            }}
+        >
+
+            <TextInput onChangeText={settext} placeholder='Enter text to copy' value={text} />
+            <Button title="Copy Text" onPress={handleCopyText} />
+            <View style={{ height: 40 }} />
+            <Button title='get copied text' onPress={handleGetCopiedText} />
+            <Text>{copied}</Text>
+        </View>
+    );
 };
 
 export default ClipboardExample;
