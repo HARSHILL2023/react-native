bd4a90e62af7fd9abfc17679abbc047560dfd83d
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-08-24 11:25:41 +0530

    uncomment code in contacts and location components for improved functionality; add share functionality with file and image picker

--- app/(drawer)/(tabs)/contacts.jsx
+++ app/(drawer)/(tabs)/contacts.jsx
@@ -1,186 +1,186 @@
-import { StyleSheet, Text, View, Button, FlatList, Image, TextInput, Pressable, Alert } from 'react-native'
-import React, {useState } from 'react'
-import * as Contacts from "expo-contacts"
+// import { StyleSheet, Text, View, Button, FlatList, Image, TextInput, Pressable, Alert } from 'react-native'
+// import React, {useState } from 'react'
+// import * as Contacts from "expo-contacts"
 
-const ContactsApp = () => {
-  const [allContacts, setAllContacts] = useState([]);
-  const [permission, setPermission] = useState(null);
-  const [searchContact, setSearchContact] = useState("");
+// const ContactsApp = () => {
+//   const [allContacts, setAllContacts] = useState([]);
+//   const [permission, setPermission] = useState(null);
+//   const [searchContact, setSearchContact] = useState("");
 
-  const handleReqPermission = async () => {
-    const contactsPermission = await Contacts.requestPermissionsAsync();
+//   const handleReqPermission = async () => {
+//     const contactsPermission = await Contacts.requestPermissionsAsync();
 
-    if (contactsPermission) {
-      setPermission(contactsPermission)
-    }
+//     if (contactsPermission) {
+//       setPermission(contactsPermission)
+//     }
 
-    if (!contactsPermission.granted) {
-      alert('Permission to access contacts was denied');
-      return;
-    }
+//     if (!contactsPermission.granted) {
+//       alert('Permission to access contacts was denied');
+//       return;
+//     }
 
-    const getContacts = await Contacts.getContactsAsync({
-      sort: Contacts.SortTypes.FirstName,
-    });
+//     const getContacts = await Contacts.getContactsAsync({
+//       sort: Contacts.SortTypes.FirstName,
+//     });
 
-    if (getContacts && getContacts.data) {
-      console.log(getContacts)
-      setAllContacts(getContacts.data);
-    }
-  }
-
-
-  const deleteContact = (item) => {
-        Alert.alert("Delete Contact",`Are you sure,delete this contact ${item.name}?`),
-        [{text:"cancel",style:"cancel"},
-          {text:"Delete",style:"destructive",onclick:async ()=>{
-            await Contacts.removeContactAsync(item.id)
-            setAllContacts((prev)=>prev.filter((ele)=>ele.id!==item.id));
-          }
-          }]
+//     if (getContacts && getContacts.data) {
+//       console.log(getContacts)
+//       setAllContacts(getContacts.data);
+//     }
+//   }
 
 
-  };
+//   const deleteContact = (item) => {
+//         Alert.alert("Delete Contact",`Are you sure,delete this contact ${item.name}?`),
+//         [{text:"cancel",style:"cancel"},
+//           {text:"Delete",style:"destructive",onclick:async ()=>{
+//             await Contacts.removeContactAsync(item.id)
+//             setAllContacts((prev)=>prev.filter((ele)=>ele.id!==item.id));
+//           }
+//           }]
 
-  const handleSearchContacts = allContacts.filter((ele) => {
-    const name = ele.name?.toLowerCase().split(' ').join('') || '';
-    const phone = ele.phoneNumbers?.[0]?.number.split(' ').join('') || '';
 
-    return (
-      name.includes(searchContact.toLowerCase().trim()) || phone.includes(searchContact)
-    );
-  });
+//   };
 
-  return (
-    <View style={styles.container}>
-      <View style={[styles.headerTop, styles.header]}>
-        <Text style={styles.title}>Contacts</Text>
-      </View>
+//   const handleSearchContacts = allContacts.filter((ele) => {
+//     const name = ele.name?.toLowerCase().split(' ').join('') || '';
+//     const phone = ele.phoneNumbers?.[0]?.number.split(' ').join('') || '';
 
-      <TextInput 
-        placeholder='Search Contact...'
-        style={styles.input}
-        value={searchContact}
-        onChangeText={setSearchContact}
-      />
+//     return (
+//       name.includes(searchContact.toLowerCase().trim()) || phone.includes(searchContact)
+//     );
+//   });
 
-      <FlatList
-        data={handleSearchContacts}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => ( 
-          <View style={styles.itemBox}>
-            <View style={styles.contactDetails}>
-              {
-                item.image ? <Image style={styles.profile} source={item.image}/> : <Text style={styles.imgBag}>{item?.firstName?.[0]}</Text>
-              }
-              <View style={styles.textContainer}>
-                <Text style={styles.itemText}>{item.name}</Text>
-                <Text style={styles.subText}>{item?.phoneNumbers?.[0]?.number || 'No number'}</Text>
-              </View>
-            </View>
+//   return (
+//     <View style={styles.container}>
+//       <View style={[styles.headerTop, styles.header]}>
+//         <Text style={styles.title}>Contacts</Text>
+//       </View>
 
-            <Pressable style={styles.deleteButton} onPress={() => deleteContact(item.id)}>
-              <Text style={styles.deleteText}>Delete</Text>
-            </Pressable>
-          </View>
-        )}
-      />
+//       <TextInput 
+//         placeholder='Search Contact...'
+//         style={styles.input}
+//         value={searchContact}
+//         onChangeText={setSearchContact}
+//       />
 
-      {permission && permission.granted === true ?
-        <Button title="Refresh Contacts" onPress={handleReqPermission} /> :
-        <Button title="Request Permission" onPress={handleReqPermission} />
-      }
-    </View>
-  )
-}
+//       <FlatList
+//         data={handleSearchContacts}
+//         keyExtractor={(item) => item.id}
+//         renderItem={({ item }) => ( 
+//           <View style={styles.itemBox}>
+//             <View style={styles.contactDetails}>
+//               {
+//                 item.image ? <Image style={styles.profile} source={item.image}/> : <Text style={styles.imgBag}>{item?.firstName?.[0]}</Text>
+//               }
+//               <View style={styles.textContainer}>
+//                 <Text style={styles.itemText}>{item.name}</Text>
+//                 <Text style={styles.subText}>{item?.phoneNumbers?.[0]?.number || 'No number'}</Text>
+//               </View>
+//             </View>
 
-export default ContactsApp
+//             <Pressable style={styles.deleteButton} onPress={() => deleteContact(item.id)}>
+//               <Text style={styles.deleteText}>Delete</Text>
+//             </Pressable>
+//           </View>
+//         )}
+//       />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 20,
-    backgroundColor: "white",
-    paddingTop: 50,
-  },
-  header: {
-    marginBottom: 16,
-    marginTop: 10,
-  },
-  headerTop: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 15,
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: '800',
-    letterSpacing: 0.5,
-  },
-  itemBox: {
-    paddingVertical: 12,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ececec',
-    flexDirection: 'row',       
-    justifyContent: 'space-between', 
-    alignItems: 'center',
-  },
-  contactDetails: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    flex: 1,
-  },
-  textContainer: {
-    marginLeft: 12,
-    flex: 1,
-  },
-  itemText: {
-    fontSize: 16,
-    fontWeight: '600',
-  },
-  subText: {
-    fontSize: 14,
-    color: '#666',
-  },
-  profile:{
-    width:40,
-    height:40,
-    borderRadius: 20,
-  },
-  imgBag:{
-    backgroundColor:"blue",
-    width:40,
-    height:40,
-    borderRadius: 20,
-    color: 'white',
-    textAlign: 'center',
-    lineHeight: 40,
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-  input: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#f5f5f5',
-    borderRadius: 8,
-    paddingHorizontal: 16,
-    fontSize: 16,
-    color: '#333333',
-    marginBottom: 16,
-    borderWidth: 1,
-    borderColor: '#e0e0e0',
-  },
+//       {permission && permission.granted === true ?
+//         <Button title="Refresh Contacts" onPress={handleReqPermission} /> :
+//         <Button title="Request Permission" onPress={handleReqPermission} />
+//       }
+//     </View>
+//   )
+// }
 
-  deleteButton: {
-    backgroundColor: '#ff3b30',
-    paddingHorizontal: 12,
-    paddingVertical: 6,
-    borderRadius: 6,
-  },
-  deleteText: {
-    color: 'white',
-    fontWeight: '600',
-    fontSize: 14,
-  }
-})
+// export default ContactsApp
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingHorizontal: 20,
+//     backgroundColor: "white",
+//     paddingTop: 50,
+//   },
+//   header: {
+//     marginBottom: 16,
+//     marginTop: 10,
+//   },
+//   headerTop: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//     marginBottom: 15,
+//   },
+//   title: {
+//     fontSize: 32,
+//     fontWeight: '800',
+//     letterSpacing: 0.5,
+//   },
+//   itemBox: {
+//     paddingVertical: 12,
+//     borderBottomWidth: 1,
+//     borderBottomColor: '#ececec',
+//     flexDirection: 'row',       
+//     justifyContent: 'space-between', 
+//     alignItems: 'center',
+//   },
+//   contactDetails: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     flex: 1,
+//   },
+//   textContainer: {
+//     marginLeft: 12,
+//     flex: 1,
+//   },
+//   itemText: {
+//     fontSize: 16,
+//     fontWeight: '600',
+//   },
+//   subText: {
+//     fontSize: 14,
+//     color: '#666',
+//   },
+//   profile:{
+//     width:40,
+//     height:40,
+//     borderRadius: 20,
+//   },
+//   imgBag:{
+//     backgroundColor:"blue",
+//     width:40,
+//     height:40,
+//     borderRadius: 20,
+//     color: 'white',
+//     textAlign: 'center',
+//     lineHeight: 40,
+//     fontWeight: 'bold',
+//     fontSize: 16,
+//   },
+//   input: {
+//     width: '100%',
+//     height: 50,
+//     backgroundColor: '#f5f5f5',
+//     borderRadius: 8,
+//     paddingHorizontal: 16,
+//     fontSize: 16,
+//     color: '#333333',
+//     marginBottom: 16,
+//     borderWidth: 1,
+//     borderColor: '#e0e0e0',
+//   },
+
+//   deleteButton: {
+//     backgroundColor: '#ff3b30',
+//     paddingHorizontal: 12,
+//     paddingVertical: 6,
+//     borderRadius: 6,
+//   },
+//   deleteText: {
+//     color: 'white',
+//     fontWeight: '600',
+//     fontSize: 14,
+//   }
+// })
---
+++ app/(drawer)/(tabs)/sharefiles.jsx
@@ -0,0 +1,77 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import React from 'react'
+import * as ImagePicketr from "expo-image-picker";
+import * as Sharing from "expo-sharing";
+import * as DocumentPicker from "expo-document-picker";
+const share = () => {
+
+    const shareFile=async()=>{
+        const res= await DocumentPicker.getDocumentAsync({
+        type: "*/*",
+        copyToCacheDirectory: true,
+      });
+        
+    if (res.canceled) {
+      return;
+    }
+    await Sharing.shareAsync(res.assets[0].uri)
+    }
+
+
+
+
+    const handleImagePicker = async () => {
+        const res = await ImagePicketr.launchImageLibraryAsync({
+            mediaTypes: ["images"],
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        console.log(res)
+
+        if (res.canceled) {
+            return;
+        }
+
+        await Sharing.shareAsync(res.assets[0].uri)
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>share</Text>
+            <Pressable style={styles.button} onPress={handleImagePicker}>
+                <Text style={styles.buttonText}>Pick Image</Text>
+            </Pressable>
+             <Pressable style={styles.button} onPress={shareFile}>
+                <Text style={styles.buttonText}>Pick File</Text>
+            </Pressable>
+        </View>
+    )
+}
+
+export default share
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        justifyContent: "center",
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginBottom: 40,
+    },
+    button: {
+        padding: 18,
+        borderRadius: 12,
+        alignItems: "center",
+        backgroundColor: "yellow"
+    },
+    buttonText: {
+        fontSize: 17,
+        fontWeight: "600",
+    },
+
+});
