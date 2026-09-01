8dd661d190aa597b3999adbdfc2aac6ad09e05fe
HARSHIL2023 <harshil.patel.cg@gmail.com>
2026-09-01 11:14:54 +0530

    refactor: update SecureStore example to use dynamic key-value input

--- app/(drawer)/(tabs)/securestore.jsx
+++ app/(drawer)/(tabs)/securestore.jsx
@@ -1,25 +1,38 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState } from 'react';
 import * as SecureStore from 'expo-secure-store';
 
 const SecureStoreExample = () => {
+  const[key,setkey]=useState(null);
+  const[value,setValue]=useState(null);
 
   const saveToken = async () => {
-    await SecureStore.setItemAsync('token', 'abc123');
+    await SecureStore.setItemAsync(key, value);
   };
 
   const getToken = async () => {
-    const token = await SecureStore.getItemAsync('token');
+    const token = await SecureStore.getItemAsync(key);
     console.log(token);
   };
 
   const deleteToken = async () => {
-    await SecureStore.deleteItemAsync('token');
+    await SecureStore.deleteItemAsync(key);
   };
 
   return (
     <View>
       <Text>Hello</Text>
+      <TextInput 
+      placeholder='enter key'
+      value={key}
+      onChangeText={setkey}
+      />
+      <TextInput 
+      placeholder='enter value'
+      value={value}
+      onChangeText={setValue}
+
+      />
 
       <Button title="Save Token" onPress={saveToken} />
       <Button title="Get Token" onPress={getToken} />
